06db241a5c4f2e536d0413de629b7d7c7aa34b82
anik587 <anik587@gmail.com>
2023-01-02 16:55:58 +0600

    66. Plus One 3rd solutions

--- LEETCODE/66. Plus One.py
+++ LEETCODE/66. Plus One.py
@@ -45,6 +45,23 @@ class Solution:
             digits[0] %= 10
             digits.insert(0, 1)
         return digits
+    
+    def pluseOne3(self, digits: List[int]) -> List[int]:
+        digits = digits[::-1]
+        carry, i = 1,0
+
+        while carry: 
+            if i < len(digits):
+                if digits[i] == 9:
+                    digits[i] = 0
+                else:
+                    digits[i]+=1
+                    carry = 0
+            else:
+                digits.append(1)
+                carry = 0
+            i+=1
+        return digits[::-1]
 
 sol = Solution()
 
