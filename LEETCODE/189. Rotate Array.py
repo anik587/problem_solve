fa404c3231a070fa6c54d4e2f4837473cded45f5
Anik Islam <anik587@gmail.com>
2023-04-28 23:47:44 +0600

    189. rotate array

--- LEETCODE/189. Rotate Array.py
+++ LEETCODE/189. Rotate Array.py
@@ -36,6 +36,9 @@
 # Try to come up with as many solutions as you can. There are at least three different ways to solve this problem.
 # Could you do it in-place with O(1) extra space?
 
+from ast import List
+
+
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
         """
