bf50d052924721a0816e360c5eb008ef43c23a2b
Anik Islam <anik587@gmail.com>
2023-04-29 22:17:54 +0600

    2656. Maximum Sum With Exactly K Elements

---
+++ text.py
@@ -0,0 +1,11 @@
+def test(ops):
+    result = None
+    res = [0]
+
+    for idx in range(len(ops)):
+        if ops[idx] == 'D':
+            res[idx -1] = res[idx-1]*2
+        else: 
+           if ops[idx] == 'D':
+        
+
